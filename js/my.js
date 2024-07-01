7b0fd23a37c34af6ca1ae55b545b1e78403baea5
abdulaziz <abdulaziz@gmail.com>
2024-07-01 20:54:17 +0500

    ..

---
+++ js/my.js
@@ -0,0 +1,11 @@
+// let button=document.querySelector('button')
+// let fgt=document.querySelector('.fgt')
+// let form =document.querySelector('form')
+
+// button.addEventListener('click',function() {
+//   form.forEach(element => {
+//     let li=document.createElement('li')
+//     li.innerHTML=element
+//     fgt.appendChild(li)
+//   });  
+// })
